9b19c5bcee8ec78a677b280d286ae4feb807041c
youssefdrif <93372842+youssefdrif@users.noreply.github.com>
2023-03-30 08:13:23 +0200

    Fixing card for personnal project tab

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,29 +4,19 @@ import weather from "../../images/weatherapp.png";
 
 export default function Profile() {
   return (
-    <div className="Container">
-      <div className="cardprojet">
-        <div className="GridMain" xs={2}>
-          <p>Projet 1</p>
-          <p>
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?"
-          </p>
-        </div>
-        <div className="GridSecond" xs={2}>
-          <img className="weather" src={weather} alt="weather" />
+    <div class="grid-container">
+      <div class="cardprofil">
+        <div class="card-content">
+          <div class="card-text">
+            <h2>Card Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              aliquam euismod ex, eget convallis risus tempus vel.
+            </p>
+          </div>
+          <div class="card-image">
+            <img src={weather} alt="Card" />
+          </div>
         </div>
       </div>
     </div>
